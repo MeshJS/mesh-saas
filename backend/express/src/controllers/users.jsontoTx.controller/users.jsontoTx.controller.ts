195f384dffde5f05ccaa1ac8d4e7f8b3c4f06446
SIDANPillow <ansonchui2608@gmail.com>
2024-10-08 16:36:26 +0800

    feat:add mainnet support

--- backend/express/src/controllers/users.jsontoTx.controller/users.jsontoTx.controller.ts
+++ backend/express/src/controllers/users.jsontoTx.controller/users.jsontoTx.controller.ts
@@ -6,17 +6,48 @@ dotenv.config();
 
 // const projectID = process.env.BLOCKFROST_PROJECT_ID!;
 
-const blockchainProvider = new BlockfrostProvider(
-  process.env.NEXT_PUBLIC_BLOCKFROST_API_KEY_PREPROD!,
-);
+// const blockchainProvider = new BlockfrostProvider(
+//   // process.env.BLOCKFROST_API_KEY_PREPROD!,
+//   process.env.BLOCKFROST_API_KEY_MAINNE!,
+// );
 
-const txBuilder = new MeshTxBuilder({
-  fetcher: blockchainProvider,
-  evaluator: blockchainProvider,
-});
+// const txBuilder = new MeshTxBuilder({
+//   fetcher: blockchainProvider,
+//   evaluator: blockchainProvider,
+// });
+
+// export const jsontoTx = async (req: Request, res: Response) => {
+//   const meshTxBody = req.body;
+
+//   try {
+//     const unsignedTx = await txBuilder.completeSync(meshTxBody);
+//     return res.json({ unsignedTx });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// };
 
 export const jsontoTx = async (req: Request, res: Response) => {
   const meshTxBody = req.body;
+  const network = meshTxBody.network;
+
+  let apiKey;
+  if (network === "preprod") {
+    apiKey = process.env.BLOCKFROST_API_KEY_PREPROD!;
+  } else if (network === "mainnet") {
+    apiKey = process.env.BLOCKFROST_API_KEY_MAINNET!;
+  } else {
+    return res
+      .status(400)
+      .json({ error: "network specified in the JSON is not supported" });
+  }
+
+  const blockchainProvider = new BlockfrostProvider(apiKey!);
+
+  const txBuilder = new MeshTxBuilder({
+    fetcher: blockchainProvider,
+    evaluator: blockchainProvider,
+  });
 
   try {
     const unsignedTx = await txBuilder.completeSync(meshTxBody);
--- backend/express/src/middleware/users.jsontoTx.validation/users.jsontoTx.middleware.test.ts
+++ backend/express/src/middleware/users.jsontoTx.validation/users.jsontoTx.middleware.test.ts
@@ -1,11 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import { validateParam } from "./users.jsontoTx.middleware";
-import noscriptTxIn_and_scriptMintJson from "../../../test/sampleJSONs/all_money_goes_back_to_change_address.json";
-import noscriptTxIn_and_scriptMintJson2 from "../../../test/sampleJSONs/send_with_output_to_specific_address.json";
-import scriptTxInJson from "../../../test/sampleJSONs/tx_spend_a_script_input.json";
-import scriptTxInJson_no_collateral from "../../../test/sampleJSONs/tx_spend_a_script_input_with_no_collateral.json";
-import scriptMintJson from "../../../test/sampleJSONs/minting_plutus_asset.json";
-import scriptMintJson_no_collateral from "../../../test/sampleJSONs/minting_plutus_asset_with_no_collateral.json";
+import noscriptTxIn_and_scriptMintJson_preprod from "../../../test/sampleJSONs/preprod/all_money_goes_back_to_change_address_preprod.json";
+import noscriptTxIn_and_scriptMintJson2_preprod from "../../../test/sampleJSONs/preprod/send_with_output_to_specific_address_preprod.json";
+import scriptTxInJson_preprod from "../../../test/sampleJSONs/preprod/tx_spend_a_script_input_preprod.json";
+import scriptTxInJson_no_collateral_preprod from "../../../test/sampleJSONs/preprod/tx_spend_a_script_input_with_no_collateral_preprod.json";
+import scriptMintJson_preprod from "../../../test/sampleJSONs/preprod/minting_plutus_asset_preprod.json";
+import scriptMintJson_no_collateral_preprod from "../../../test/sampleJSONs/preprod/minting_plutus_asset_with_no_collateral_preprod.json";
+import noscriptTxIn_and_scriptMintJson_mainnet from "../../../test/sampleJSONs/mainnet/all_money_goes_back_to_change_address_mainnet.json";
+// import scriptTxInJson_mainnet from "../../../test/sampleJSONs/mainnet/tx_spend_a_script_input_mainnet.json";
+// import scriptMintJson_mainnet from "../../../test/sampleJSONs/mainnet/minting_plutus_asset_mainnet.json";
+// import noscriptTxIn_and_scriptMintJson2_mainnet from "../../../test/sampleJSONs/mainnet/send_with_output_to_specific_address_mainnet.json";
+// import scriptMintJson_no_collateral_mainnet from "../../../test/sampleJSONs/mainnet/minting_plutus_asset_with_no_collateral_mainnet.json";
+// import scriptTxInJson_no_collateral_mainnet from "../../../test/sampleJSONs/mainnet/tx_spend_a_script_input_with_no_collateral_mainnet.json";
 
 describe("validateParam Middleware", () => {
   let req: Request;
@@ -20,26 +26,26 @@ describe("validateParam Middleware", () => {
     next = jest.fn();
   });
 
-  it("should pass for JSON file with no ScriptTxIn and ScriptSource", async () => {
-    req.body = noscriptTxIn_and_scriptMintJson;
+  it("should pass for JSON file with no ScriptTxIn and ScriptSource - preprod", async () => {
+    req.body = noscriptTxIn_and_scriptMintJson_preprod;
     await validateParam(req, res, next);
     expect(next).toHaveBeenCalled();
   });
 
-  it("should pass for JSON file with no ScriptTxIn and ScriptSource 2", async () => {
-    req.body = noscriptTxIn_and_scriptMintJson2;
+  it("should pass for JSON file with no ScriptTxIn and ScriptSource 2 - preprod", async () => {
+    req.body = noscriptTxIn_and_scriptMintJson2_preprod;
     await validateParam(req, res, next);
     expect(next).toHaveBeenCalled();
   });
 
-  it("should pass for JSON file with both ScriptTxIn and collaterals", async () => {
-    req.body = scriptTxInJson;
+  it("should pass for JSON file with both ScriptTxIn and collaterals - preprod", async () => {
+    req.body = scriptTxInJson_preprod;
     await validateParam(req, res, next);
     expect(next).toHaveBeenCalled();
   });
 
-  it("should fail for JSON file with ScriptTxIn and without collaterals", async () => {
-    req.body = scriptTxInJson_no_collateral;
+  it("should fail for JSON file with ScriptTxIn and without collaterals - preprod", async () => {
+    req.body = scriptTxInJson_no_collateral_preprod;
     await validateParam(req, res, next);
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.json).toHaveBeenCalledWith({
@@ -48,14 +54,14 @@ describe("validateParam Middleware", () => {
     });
   });
 
-  it("should pass for JSON file with both ScriptMint and collaterals", async () => {
-    req.body = scriptMintJson;
+  it("should pass for JSON file with both ScriptMint and collaterals - preprod", async () => {
+    req.body = scriptMintJson_preprod;
     await validateParam(req, res, next);
     expect(next).toHaveBeenCalled();
   });
 
-  it("should fail for JSON file with both ScriptTxIn and ScriptSource and no collaterals", async () => {
-    req.body = scriptMintJson_no_collateral;
+  it("should fail for JSON file with both ScriptTxIn and ScriptSource and no collaterals - preprod", async () => {
+    req.body = scriptMintJson_no_collateral_preprod;
     await validateParam(req, res, next);
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.json).toHaveBeenCalledWith({
@@ -63,4 +69,48 @@ describe("validateParam Middleware", () => {
         "Collaterals cannot be null when scriptTxIn or scriptMint is present.",
     });
   });
+
+  it("should pass for JSON file with no ScriptTxIn and ScriptSource - mainnet", async () => {
+    req.body = noscriptTxIn_and_scriptMintJson_mainnet;
+    await validateParam(req, res, next);
+    expect(next).toHaveBeenCalled();
+  });
+
+  // it("should pass for JSON file with both ScriptTxIn and collaterals - mainnet", async () => {
+  //   req.body = scriptTxInJson_mainnet;
+  //   await validateParam(req, res, next);
+  //   expect(next).toHaveBeenCalled();
+  // });
+
+  // it("should pass for JSON file with both ScriptMint and collaterals - mainnet", async () => {
+  //   req.body = scriptMintJson_mainnet;
+  //   await validateParam(req, res, next);
+  //   expect(next).toHaveBeenCalled();
+  // });
+
+  // it("should pass for JSON file with no ScriptTxIn and ScriptSource 2 - mainnet", async () => {
+  //   req.body = noscriptTxIn_and_scriptMintJson2_mainnet;
+  //   await validateParam(req, res, next);
+  //   expect(next).toHaveBeenCalled();
+  // });
+
+  // it("should fail for JSON file with ScriptTxIn but without collateral - mainnet", async () => {
+  //   req.body = scriptTxInJson_no_collateral_mainnet;
+  //   await validateParam(req, res, next);
+  //   expect(res.status).toHaveBeenCalledWith(400);
+  //   expect(res.json).toHaveBeenCalledWith({
+  //     error:
+  //       "Collaterals cannot be null when scriptTxIn or scriptMint is present.",
+  //   });
+  // });
+
+  // it("should fail for JSON file with ScriptMint but without collateral - mainnet", async () => {
+  //   req.body = scriptMintJson_no_collateral_mainnet;
+  //   await validateParam(req, res, next);
+  //   expect(res.status).toHaveBeenCalledWith(400);
+  //   expect(res.json).toHaveBeenCalledWith({
+  //     error:
+  //       "Collaterals cannot be null when scriptTxIn or scriptMint is present.",
+  //   });
+  // });
 });
